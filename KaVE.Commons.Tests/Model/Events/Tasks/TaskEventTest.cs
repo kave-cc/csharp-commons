55bd29ea8ca05263471a115f5b127a42b6cca272
Sebastian Proksch <development@mail.proks.ch>
2017-11-23 16:59:53 +0100

    Create TaskEvent.

---
+++ KaVE.Commons.Tests/Model/Events/Tasks/TaskEventTest.cs
@@ -0,0 +1,164 @@
+﻿/*
+ * Copyright 2017 University of Zurich
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using KaVE.Commons.Model.Events.Enums;
+using KaVE.Commons.Model.Events.Tasks;
+using NUnit.Framework;
+
+namespace KaVE.Commons.Tests.Model.Events.Tasks
+{
+    internal class TaskEventTest
+    {
+        [Test]
+        public void DefaultValues()
+        {
+            var sut = new TaskEvent("p", "t", TaskAction.Create);
+            Assert.AreEqual("p", sut.Version);
+            Assert.AreEqual("t", sut.TaskId);
+            Assert.AreEqual(TaskAction.Create, sut.Action);
+
+            Assert.Null(sut.NewParentId);
+            Assert.Null(sut.Annoyance);
+            Assert.Null(sut.Importance);
+            Assert.Null(sut.Urgency);
+        }
+
+        [Test]
+        public void SettingValues()
+        {
+            var sut = new TaskEvent("p2", "t2", TaskAction.Activate)
+            {
+                NewParentId = "x",
+                Annoyance = Likert5Point.Negative1,
+                Importance = Likert5Point.Neutral,
+                Urgency = Likert5Point.Positive1
+            };
+            Assert.AreEqual("p2", sut.Version);
+            Assert.AreEqual("t2", sut.TaskId);
+            Assert.AreEqual(TaskAction.Activate, sut.Action);
+
+            Assert.AreEqual("x", sut.NewParentId);
+            Assert.AreEqual(Likert5Point.Negative1, sut.Annoyance);
+            Assert.AreEqual(Likert5Point.Neutral, sut.Importance);
+            Assert.AreEqual(Likert5Point.Positive1, sut.Urgency);
+        }
+
+        [Test]
+        public void Equality_Default()
+        {
+            var a = new TaskEvent("p2", "t2", TaskAction.Activate);
+            var b = new TaskEvent("p2", "t2", TaskAction.Activate);
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_SettingValues()
+        {
+            var a = new TaskEvent("p2", "t2", TaskAction.Activate)
+            {
+                NewParentId = "x",
+                Annoyance = Likert5Point.Negative1,
+                Importance = Likert5Point.Neutral,
+                Urgency = Likert5Point.Positive1
+            };
+            var b = new TaskEvent("p2", "t2", TaskAction.Activate)
+            {
+                NewParentId = "x",
+                Annoyance = Likert5Point.Negative1,
+                Importance = Likert5Point.Neutral,
+                Urgency = Likert5Point.Positive1
+            };
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentVersion()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate);
+            var b = new TaskEvent("p2", "t", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentTask()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate);
+            var b = new TaskEvent("p", "t2", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentAction()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate);
+            var b = new TaskEvent("p", "t", TaskAction.Pause);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentParent()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate)
+            {
+                NewParentId = "x"
+            };
+            var b = new TaskEvent("p", "t", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentDifferentAnnoyance()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate)
+            {
+                Annoyance = Likert5Point.Negative1
+            };
+            var b = new TaskEvent("p", "t", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentImportance()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate)
+            {
+                Importance = Likert5Point.Neutral
+            };
+            var b = new TaskEvent("p", "t", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_DifferentUrgency()
+        {
+            var a = new TaskEvent("p", "t", TaskAction.Activate)
+            {
+                Urgency = Likert5Point.Positive1
+            };
+            var b = new TaskEvent("p", "t", TaskAction.Activate);
+            Assert.AreNotEqual(a, b);
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+    }
+}
---
+++ KaVE.Commons/Model/Events/Tasks/TaskEvent.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Copyright 2017 Nico Strebel
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System.Runtime.Serialization;
+using KaVE.Commons.Model.Events.Enums;
+using KaVE.Commons.Utils;
+using KaVE.JetBrains.Annotations;
+
+namespace KaVE.Commons.Model.Events.Tasks
+{
+    public interface ITaskEvent
+    {
+        [NotNull]
+        string Version { get; set; }
+
+        [NotNull]
+        string TaskId { get; set; }
+
+        TaskAction Action { get; set; }
+
+        /// <summary>
+        ///     Set for create and move actions.
+        /// </summary>
+        [CanBeNull]
+        string NewParentId { get; set; }
+
+        /// <summary>
+        ///     Set for create and edit actions.
+        /// </summary>
+        [CanBeNull]
+        Likert5Point? Annoyance { get; set; }
+
+        /// <summary>
+        ///     Set for create and edit actions.
+        /// </summary>
+        [CanBeNull]
+        Likert5Point? Importance { get; set; }
+
+        /// <summary>
+        ///     Set for create and edit actions.
+        /// </summary>
+        [CanBeNull]
+        Likert5Point? Urgency { get; set; }
+    }
+
+    [DataContract]
+    public class TaskEvent : IDEEvent, ITaskEvent
+    {
+        public TaskEvent(string version, string taskId, TaskAction action)
+        {
+            Version = version;
+            TaskId = taskId;
+            Action = action;
+        }
+
+        [DataMember]
+        public string Version { get; set; }
+
+        [DataMember]
+        public string TaskId { get; set; }
+
+        [DataMember]
+        public TaskAction Action { get; set; }
+
+        [DataMember]
+        public string NewParentId { get; set; }
+
+        [DataMember]
+        public Likert5Point? Annoyance { get; set; }
+
+        [DataMember]
+        public Likert5Point? Importance { get; set; }
+
+        [DataMember]
+        public Likert5Point? Urgency { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj, Equals);
+        }
+
+        internal bool Equals(TaskEvent other)
+        {
+            return base.Equals(other) && string.Equals(Version, other.Version) && string.Equals(TaskId, other.TaskId) &&
+                   Action == other.Action && string.Equals(NewParentId, other.NewParentId) &&
+                   Annoyance == other.Annoyance && Importance == other.Importance && Urgency == other.Urgency;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ Version.GetHashCode();
+                hashCode = (hashCode * 397) ^ TaskId.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int) Action;
+                hashCode = (hashCode * 397) ^ (NewParentId != null ? NewParentId.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Annoyance.GetHashCode();
+                hashCode = (hashCode * 397) ^ Importance.GetHashCode();
+                hashCode = (hashCode * 397) ^ Urgency.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
